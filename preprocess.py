d0d90c90cd5e371799041d2686601843f3c6cae1
uday <udayteja96@gmail.com>
2018-04-28 15:41:00 +0530

    Code and Data Files

---
+++ preprocess.py
@@ -0,0 +1,146 @@
+from pynlp import StanfordCoreNLP
+import spacy
+from neuralcoref import Coref
+
+spacy_parser = spacy.load('en')
+
+annotators = "tokenize, ssplit, pos, lemma, ner, parse, dcoref"
+options = {}
+
+nlp = StanfordCoreNLP(annotators=annotators, options=options)
+
+def split_parts(first_part,is_first_part):
+	first_parse = spacy_parser(first_part)
+	has_verb = False
+	verb_str = None
+	P1=""
+	V1=""
+	A1=""
+	Pr1=""
+	for token in first_parse:
+		if token.pos_ == "VERB":
+			has_verb = True
+			verb_str = str(token.text)
+			break
+	if not has_verb and is_first_part:
+		print("No Verb found in first part")
+		return P1,V1,A1,Pr1
+	elif not has_verb and not is_first_part:
+		after_verb = first_part
+	else:
+		V1 = verb_str
+		P1 = first_part.split(V1)[0].strip()
+		after_verb = first_part.split(V1)[1].strip()
+	prep_parse = spacy_parser(after_verb)
+	has_prep = False
+	prep_str = None
+	for token in prep_parse:
+		if (token.tag_ == "IN" or token.tag_ == "TO") and str(token.text)!="by":
+			has_prep = True
+			prep_str = str(token.text)
+			break
+	if not has_prep:
+		A1 = after_verb
+	else:
+		A1 = after_verb.split(prep_str)[0].strip()
+		Pr1 = prep_str+" "+after_verb.split(prep_str)[1].strip()
+	return P1,V1,A1,Pr1
+
+def resolve_conjs(text):
+	print("Resolving Conjunctions...")
+	document = nlp(text)
+	document2 = spacy_parser(text)
+	has_conj = False
+	conj_str = None
+	conjs = ["and","but","if","then"]
+	for conj in conjs:
+		if conj in text:
+			has_conj = True
+			conj_str = conj
+			break
+	if not has_conj:
+		print("conjuction not found")
+		return text,""
+	parts = text.split(conj_str)
+	if len(parts) != 2:
+		print("Error")
+	first_part = parts[0].strip()
+	second_part = parts[1].strip()
+	P1,V1,A1,Pr1 = split_parts(first_part,True)
+	P2,V2,A2,Pr2 = split_parts(second_part,False)
+	if len(P1)==0:
+		P1 = P2
+	if len(P2)==0:
+		P2 = P1
+	if len(V1)==0:
+		V1 = V2
+	if len(V2)==0:
+		V2 = V1
+	if len(A1)==0:
+		A1 = A2
+	if len(P2)==0:
+		A2 = A1
+	if len(Pr1)==0:
+		Pr1 = Pr2
+	if len(Pr2)==0:
+		Pr2 = Pr1
+	first_part_new = (P1+" "+V1+" "+A1+" "+Pr1).strip()
+	second_part_new = (P2+" "+V2+" "+A2+" "+Pr2).strip()
+	if "." not in first_part_new:
+		first_part_new += "."
+	if "." not in second_part_new:
+		second_part_new += "."
+	return first_part_new,second_part_new
+
+def resolve_dollars(text):
+	print("Resolving Dollars...")
+	if "$" not in text:
+		return text
+	parts = text.split()
+	processed_text = ""
+	for part in parts:
+		if "$" not in part:
+			processed_text += part+" "
+		else:
+			processed_text += part.replace("$","")+" dollars "
+	return processed_text.strip()
+
+def resolve_corefs(text):
+	print("Resolving Co-Refs...")
+	document = nlp(text)
+	coref = Coref()
+	context = ""
+	for sentence in document:
+		print(str(sentence))
+		clusters = coref.one_shot_coref(utterances=str(sentence).strip(), context=context)
+		resolved_utterance_text = coref.get_resolved_utterances()
+		print(resolved_utterance_text)
+		context += " ".join(resolved_utterance_text).strip()
+	return context
+
+def preprocess_text(text):
+	document = nlp(text)
+	processed_text = ""
+	for sentence in document:
+		first_part_new,second_part_new = resolve_conjs(str(sentence))
+		if len(first_part_new)>0:
+			processed_text += resolve_dollars(first_part_new)
+		if len(second_part_new)>0:
+			processed_text += resolve_dollars(second_part_new)
+	processed_text = processed_text.replace(" . ",".")
+	processed_text = processed_text.replace("."," . ")
+	processed_text = resolve_corefs(processed_text)
+	processed_text = processed_text.replace(" . ",".")
+	processed_text = processed_text.replace(" .",".")
+	processed_text = processed_text.replace(". ",".")
+	processed_text = processed_text.replace("."," . ")
+	return processed_text
+
+
+if __name__ == "__main__":
+	text = "Sam had 49 pennies and 34 nickels in his bank ."
+	text = 'Sara has 31 red and 15 green balloons . Sandy has 24 red balloons . How many red balloons do they have in total ? '
+	# text = 'Jason found 49 seashells and 48 starfish on the beach . He gave 13 of the seashells to Tim . How many seashells does Jason now have ? '
+	# text = 'Liz had 9 black kittens. She gave some of her kittens to Joan. Joan has now 11 kittens. Liz has 5 kittens left and 3 has spots. How many kittens did Joan get?'
+	processed_text = preprocess_text(text)
+	print(processed_text)
---
+++ word_prob_solver.py
@@ -0,0 +1,334 @@
+from pynlp import StanfordCoreNLP
+import spacy
+from nltk.parse.stanford import StanfordDependencyParser
+from nltk.parse.stanford import StanfordParser
+from nltk.tree import *
+from preprocess import preprocess_text
+import re
+from copy import deepcopy
+
+def chunk_tree_to_sent(tree, concat=' '):
+	punct_re = re.compile(r'\s([,\.;\?])')
+	s = concat.join([t for t in tree.leaves()])
+	return re.sub(punct_re, r'\g<1>', s)
+
+def ExtractNounPhrases( myTree, phrase="NP"):
+	myPhrases = []
+	if (myTree.label() == phrase):
+		has_child_NP = False
+		for child in myTree:
+			if(child.label() == phrase):
+				has_child_NP = True
+				break
+		if not has_child_NP:
+			myPhrases.append( myTree.copy(True) )
+	for child in myTree:
+		if (type(child) is Tree):
+			list_of_phrases = ExtractNounPhrases(child, phrase)
+			if (len(list_of_phrases) > 0):
+				myPhrases.extend(list_of_phrases)
+	return myPhrases
+
+def get_numbers(document):
+	print("identifying numbers...")
+	sentences = []
+	numbers = []
+	for index, sentence in enumerate(document):
+		a = [[str(entity),str(sentence),index] for entity in sentence.entities if str(entity.type) == 'NUMBER']
+		numbers.extend(a)
+		sentences.append(str(sentence))
+	return sentences,numbers
+
+def get_num_dep_nouns(document,dep_parser):
+	print("identifying nouns dep numbers...")
+	h = set([])
+	for sentence in document:
+		print(sentence)
+		result = dep_parser.raw_parse(str(sentence))
+		for parse in result:
+			for dep in list(parse.triples()):
+				if dep[1] == "nummod":
+					h.update([str(dep[0][0])])
+	h = list(h)
+	return h
+
+def get_noun_phrases_entities(h,sentences,tree_parser):
+	print("identifying NPs & et...")
+	NPs=[]
+	et=[]
+	parse_trees = sum([list(dep_graphs) for dep_graphs in tree_parser.raw_parse_sents(sentences)], []) 
+	for sid in range(len(parse_trees)):
+		tree = parse_trees[sid]
+		NP_tree_list = ExtractNounPhrases(tree, 'NP')
+		for np in NP_tree_list:
+			np_str = chunk_tree_to_sent(np)
+			NPs.append([np_str,sentences[sid]])
+			for h_noun in h:
+				if h_noun in np_str:
+					et.append([np_str,h_noun,sentences[sid]])
+	return NPs,et
+
+def get_verbs(et,spacy_parser):
+	print("identifying vt...")
+	vt=[]
+	for entity in et:
+		spacy_pos = spacy_parser(entity[2])
+		for token in spacy_pos:
+			if(token.pos_) == "VERB":
+				vt.append([token.text,entity[1],entity[2]])
+				break
+	return vt
+
+def get_ex_vx(document2,sentences,h):
+	print("identifying ex & vx...")
+	ex = None
+	vx = None
+	for np in document2.noun_chunks:
+		if(np.root.text in h):
+			ex = [np.text,np.root.text,sentences[-1]]
+			x = np.root.head
+			while(str(x.pos_)!="VERB"):
+				x = x.head
+			vx = [x.text,np.root.text]
+	return ex,vx
+
+def get_numt(et,numbers):
+	print("identifying numt...")
+	numt = []
+	for chunk in et:
+		flag = False
+		for num in numbers:
+			if num[0] in chunk[0] and chunk[0] in num[1]:
+				flag = True
+				numt.append([num[0],chunk[1],chunk[2]])
+				numbers.remove(num)
+				break
+		if not flag:
+			numt.append(['$',chunk[1],chunk[2]])
+	return numt
+
+def process_bare_num(numbers,sentences,numt,et,vt,h):
+	if len(numbers)==0:
+		return
+	print("bare numbers found...")
+	for num in numbers:
+		h_noun_back = None
+		h_noun_front = None
+		back_ind = 1000
+		front_ind = 1000
+		dist = 0	#backward
+		parts = num[1].split()
+		nind = parts.index(num[0])
+		flag = False
+		for i in range(nind-1,-1,-1):
+			dist += 1
+			if parts[i] in h:
+				flag = True
+				h_noun_back = parts[i]
+				break
+		if not flag:
+			for j in range(num[2]-1,-1,-1):
+				parts = str(sentences[j]).split()
+				for i in range(len(parts)-1,-1,-1):
+					dist += 1
+					if parts[i] in h:
+						flag = True
+						h_noun_back = parts[i]
+						break
+				if flag:
+					break
+		back_ind = dist
+		dist = 0	#forward
+		parts = num[1].split()
+		nind = parts.index(num[0])
+		flag = False
+		for i in range(nind+1,len(parts),1):
+			dist += 1
+			if parts[i] in h:
+				flag = True
+				h_noun_front = parts[i]
+				break
+		if not flag:
+			for j in range(num[2]+1,len(sentences),1):
+				parts = str(sentences[j]).split()
+				for i in range(0,len(parts),1):
+					dist += 1
+					if parts[i] in h:
+						flag = True
+						h_noun_front = parts[i]
+						break
+				if flag:
+					break
+		front_ind = dist
+		h_noun_num = None
+		if h_noun_back and h_noun_front:
+			if front_ind < back_ind:
+				h_noun_num = h_noun_front
+			else:
+				h_noun_num = h_noun_back
+		elif h_noun_back:
+			h_noun_num = h_noun_back
+		elif h_noun_front:
+			h_noun_num = h_noun_front
+		if h_noun_num:
+			print([num[0],h_noun_num])
+			update_existing_numt = False
+			for enum in numt:
+				if(enum[0] == "$" and enum[1] == h_noun_num and enum[2] == num[1]):
+					update_existing_numt = True
+					enum[0] = num[0]
+					break
+			if not update_existing_numt:
+				numt.append([num[0],h_noun_num,num[1]])
+				et.append([num[0]+"|"+h_noun_num,h_noun_num,num[1]])
+				vt.append([num[0]+"|"+h_noun_num,h_noun_num,num[1]])
+		else:
+			print("No entity found for bare number: "+str(num[0]))
+
+def get_attributes(et,ex,nlp):
+	print("identifying ats...")
+	at = []
+	last_at = "$"
+	for e in et+[ex]:
+		d = nlp(e[0])
+		flag = False
+		for token in d[0]:
+			if 'JJ' in token.pos and str(token) != "many":
+				flag = True
+				at.append([str(token),e[1],e[2]])
+				last_at = str(token)
+				break
+		if not flag:
+			print("No attribute found for entity: "+str(e[1]))
+			at.append([last_at,e[1],e[2]])
+	print("identifying ax...")
+	ax = at[-1]
+	del at[-1]
+	return at,ax
+
+def get_fragments(et,numt,vt,at,ex,vx,ax):
+	print("identifying fragments...")
+	fragments = []
+	for e,n,v,a  in zip(et,numt,vt,at):
+		print(e,n,v,a,"\n",sep="\n")
+		fragments.append([e[2],e[1],e[0],n[0],v[0],a[0]])
+	fragments.append([ex[2],ex[1],ex[0],"$",vx[0],ax[0]])
+	return fragments
+
+def get_containers(fragments,dep_parser):
+	print("identifying ct...")
+	ct=[]
+	last_ct="$"
+	for fragment in fragments:
+		result = dep_parser.raw_parse(fragment[0])
+		ct1="$"
+		ct2="$"
+		for parse in result:
+			for dep in list(parse.triples()):
+				if dep[1] == "nsubj" and dep[0][0] == fragment[4]:
+					if ct1 == "$":
+						ct1 = dep[2][0]
+						last_ct = ct1
+				elif dep[1] == "iobj" and dep[0][0] == fragment[4]:
+					if ct2 == "$":
+						ct2 = dep[2][0]
+						last_ct = ct1
+				elif dep[1] == "nmod" and dep[0][0] == fragment[4]:
+					if ct2 == "$":
+						ct2 = dep[2][0]
+						last_ct = ct1
+		if ct1 == "$":
+			ct1 = last_ct
+		if ct2 == "$":
+			ct2 = last_ct
+		ct.append([ct1,ct2,fragment[0]])
+		fragment.append([ct1,ct2])
+	return ct
+
+#Sentence Verb Categorization
+def verb_category(verb):
+	verb = verb.lower()
+	OBS_verbs = ["has","had","found"]
+	NEG_TR_verbs = ["give","gave"]
+	if verb in OBS_verbs:
+		return "OBS"
+	elif verb in NEG_TR_verbs:
+		return "NEG_TR"
+
+def get_states(fragments,verb_cats,ex,ax):
+	#State Progression
+	states=[]
+	for fragment,vcat in zip(fragments,verb_cats):
+		state={}
+		if len(states) > 0:
+			state = deepcopy(states[-1])
+		if fragment[1] == ex[1] and fragment[5] == ax[0]:
+			if vcat == "OBS":
+				if fragment[6][0].lower() not in state:
+					state[fragment[6][0].lower()] = {"N":fragment[3],"E":fragment[1],"A":fragment[5]}
+				else:
+					state[fragment[6][0].lower()]["N"] = fragment[3]
+			elif vcat == "NEG_TR":
+				if fragment[6][0].lower() not in state:
+					state[fragment[6][0].lower()] = {"N":"J0-"+fragment[3],"E":fragment[1],"A":fragment[5]}
+				else:
+					state[fragment[6][0].lower()]["N"] += "-"+fragment[3]
+				if fragment[6][1].lower() not in state:
+					state[fragment[6][1].lower()] = {"N":"J0+"+fragment[3],"E":fragment[1],"A":fragment[5]}
+				else:
+					state[fragment[6][1].lower()]["N"] += "+"+fragment[3]
+		states.append(state)
+	return states
+
+def init_parsers():
+	print("initializing parsers...")
+	spacy_parser = spacy.load('en')
+	path_to_jar = './stanford-corenlp-full-2018-02-27/stanford-corenlp-3.9.1.jar'
+	path_to_models_jar = './stanford-corenlp-full-2018-02-27/stanford-corenlp-3.9.1-models.jar'
+	dep_parser = StanfordDependencyParser(path_to_jar=path_to_jar, path_to_models_jar=path_to_models_jar)
+	tree_parser = StanfordParser(path_to_jar=path_to_jar, path_to_models_jar=path_to_models_jar)
+	annotators = "tokenize, ssplit, pos, lemma, ner, parse, dcoref"
+	options = {}
+	nlp = StanfordCoreNLP(annotators=annotators, options=options)
+	return nlp,spacy_parser,dep_parser,tree_parser
+
+def word_prob_solver(text):
+	nlp,spacy_parser,dep_parser,tree_parser = init_parsers()	
+	text = preprocess_text(text)
+	print(text)
+	document = nlp(text)
+	document2 = spacy_parser(text)
+	sentences,numbers = get_numbers(document)
+	h = get_num_dep_nouns(document,dep_parser)
+	NPs,et = get_noun_phrases_entities(h,sentences,tree_parser)
+	vt = get_verbs(et,spacy_parser)
+	ex,vx = get_ex_vx(document2,sentences,h)
+	numt = get_numt(et,numbers)
+	process_bare_num(numbers,sentences,numt,et,vt,h)
+	at,ax = get_attributes(et,ex,nlp)
+	fragments = get_fragments(et,numt,vt,at,ex,vx,ax)
+	assert len(fragments) == len(sentences)
+	ct = get_containers(fragments,dep_parser)
+	for fragment in fragments:
+		print(fragment,"\n")
+	fragx = fragments[-1]
+	del fragments[-1]
+	verb_cats = []
+	for fragment in fragments:
+		verb_cats.append(verb_category(fragment[4]))
+	# verb_cats = ["OBS","NEG_TR","OBS","OBS","$"]
+	# verb_cats = ["OBS","NEG_TR","OBS"]
+	states = get_states(fragments,verb_cats,ex,ax)
+	for state in states:
+		print(state,"\n")
+
+if __name__ == "__main__":
+	# text = 'Joan found 70 seashells on the beach . she gave Sam some of her seashells . She has 27 seashells . How many seashells did she give to Sam ?'
+	text = 'Liz had 9 black kittens. She gave some of her kittens to Joan. Joan has now 11 kittens. Liz has 5 kittens left and 3 has spots. How many kittens did Joan get?'
+	# text = 'Jason found 49 seashells and 48 starfish on the beach . He gave 13 of the seashells to Tim . How many seashells does Jason now have ? '
+	# TODO
+	# text = 'Sara has 31 red and 15 green balloons . Sandy has 24 red balloons . How many red balloons do they have in total ? '
+	# text = 'There are 42 walnut trees and 12 orange trees currently in the park. Park workers cut down 13 walnut trees that were damaged. How many walnut trees will be in the park when the workers are finished?'
+	# text = 'Joan went to 4 football games this year. She went to 9 games last year. How many football games did Joan go?'
+	# text = 'There were 28 bales of hay in the barn . Tim stacked bales in the barn today . There are now 54 bales of hay in the barn . How many bales did he store in the barn ? '
+	word_prob_solver(text)
