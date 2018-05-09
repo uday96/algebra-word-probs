c2f1feaaef23fce2fa7bdace45833399a3971975
uday <udayteja96@gmail.com>
2018-05-09 20:35:55 +0530

    changing file structure

--- pynlp_server.py
+++
@@ -1,8 +0,0 @@
-import os
-
-HOME_PATH = "/home/uday"
-
-os.environ["CORE_NLP"] = HOME_PATH+"/NLP/algebra-word-probs/algebra-word-probs/stanford-corenlp-full-2018-02-27"
-# os.system("export CORE_NLP=${HOME}/NLP/algebra-word-probs/algebra-word-probs/stanford-corenlp-full-2018-02-27")
-
-os.system("python3 -m pynlp")
