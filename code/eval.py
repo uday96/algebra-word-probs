c8e80b1a370078a820d29b5b84ae11fedab666ae
uday <udayteja96@gmail.com>
2018-05-09 23:34:46 +0530

    results

--- code/eval.py
+++ code/eval.py
@@ -1,6 +1,33 @@
-from word_prob_solver import word_prob_solver
 import json
+from word_prob_solver import word_prob_solver
 from helper import HiddenPrints
+
+def eval_model_from_json():
+	# PATH = "./data/DfN/ALL/ALL.json"
+	PATH = "./data/DS3/DS3.json"
+	dataset = json.load(open(PATH,"r"))
+	correct_fp = open("./results/DS3/correct.txt","w")
+	wrong_fp = open("./results/DS3/wrong.txt","w")
+	correct = 0
+	count = 0
+	for data in dataset:
+		print(count)
+		try:
+			ans = word_prob_solver(str(data["sQuestion"]).strip())
+			if str(ans) == str(data["lSolutions"][0]).strip():
+				correct += 1
+				correct_fp.write(str(data["sQuestion"])+"\n"+str(ans)+"\n\n")
+			else:
+				wrong_fp.write(str(data["sQuestion"])+"\n"+str(ans)+"\n\n")	
+		except Exception as e:
+			wrong_fp.write(str(data["sQuestion"])+"\n"+str(e)+"\n\n")
+		finally :
+			count += 1
+	correct_fp.close()
+	wrong_fp.close()
+	print("correct: ",str(correct))
+	print("total: ",str(count))
+	print("acc: ",str(float(correct)/count))
 
 def eval_model_from_file():
 	with open("../data/questions_refined.txt") as f:
@@ -33,7 +60,4 @@ def eval_model_from_file():
 	print("acc: ",str((float(count1)*100)/count2))
 
 if __name__ == "__main__":
-	eval_model_from_file()
-	# correct:  66
-	# total:  141
-	# acc:  46.808510638297875
+	eval_model_from_json()
